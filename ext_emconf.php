768402e6cb127049dee7d3052f0e44225d6778f6
Pascale Beier <mail@pascalebeier.de>
2017-04-07 15:08:05 +0200

    Switch to PHP 5.4+ Style Arrays

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,25 +1,23 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
-    'title'          => 'RSS2 Import Presse-Service',
-    'description'    => 'Importiert RSS2 Feeds aus dem Presse-Service in die Extension tt_news',
-    'module'         => 'mod1',
-    'state'          => 'beta',
-    'version'        => '7.0.1',
-    'author'         => 'Kasper Ligaard, Morten Tranberg Hansen, Mads Kirkedal Henriksen, Andreas Wietfeld und Pascale Beier',
-    'author_email'   => 'info@ruhr-connect.de',
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'RSS2 Import Presse-Service',
+    'description' => 'Importiert RSS2 Feeds aus dem Presse-Service in die Extension tt_news',
+    'module' => 'mod1',
+    'state' => 'beta',
+    'version' => '7.0.1',
+    'author' => 'Kasper Ligaard, Morten Tranberg Hansen, Mads Kirkedal Henriksen, Andreas Wietfeld und Pascale Beier',
+    'author_email' => 'info@ruhr-connect.de',
     'author_company' => 'ruhr-connect GmbH',
-    'constraints'    => array(
-        'depends'   => array(
-            'php'   => '5.6.0-7.0.99',
+    'constraints' => [
+        'depends' => [
+            'php' => '5.6.0-7.0.99',
             'typo3' => '7.2.0-7.99.99',
-        ),
-        'conflicts' => array(),
-        'suggests'  => array(
-            'tt_news'   => '7.0.0-7.99.99',
+        ],
+        'conflicts' => [],
+        'suggests' => [
+            'tt_news' => '7.0.0-7.99.99',
             'scheduler' => '7.2.0-7.99.99',
-        ),
-    ),
-);
-
-?>
+        ],
+    ],
+];
