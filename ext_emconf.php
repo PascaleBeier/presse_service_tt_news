86b4aefc0aaac7fd3aa5448d6b92a80b94b50c11
Pascale Beier <mail@pascalebeier.de>
2017-04-07 15:05:11 +0200

    Update ext_emconf.php for v7

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,19 +5,19 @@ $EM_CONF[$_EXTKEY] = array(
     'description'    => 'Importiert RSS2 Feeds aus dem Presse-Service in die Extension tt_news',
     'module'         => 'mod1',
     'state'          => 'beta',
-    'version'        => '6.0.1',
+    'version'        => '7.0.1',
     'author'         => 'Kasper Ligaard, Morten Tranberg Hansen, Mads Kirkedal Henriksen, Andreas Wietfeld und Pascale Beier',
     'author_email'   => 'info@ruhr-connect.de',
     'author_company' => 'ruhr-connect GmbH',
     'constraints'    => array(
         'depends'   => array(
-            'php'   => '5.2.11-5.6.99',
-            'typo3' => '6.2.0-6.99.99',
+            'php'   => '5.6.0-7.0.99',
+            'typo3' => '7.2.0-7.99.99',
         ),
         'conflicts' => array(),
         'suggests'  => array(
-            'tt_news'   => '3.0.0-3.99.99',
-            'scheduler' => '6.2.0-6.99.99',
+            'tt_news'   => '7.0.0-7.99.99',
+            'scheduler' => '7.2.0-7.99.99',
         ),
     ),
 );
